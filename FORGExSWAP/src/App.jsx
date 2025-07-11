e3fc5387a237443cb36d1affe5cbb11f8fa67fd6
surajsbhoj0101 <surajsbhoj0101@gmail.com>
2025-07-11 20:10:12 +0530

    Fixed logo image

--- FORGExSWAP/src/App.jsx
+++ FORGExSWAP/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { FaArrowCircleRight } from "react-icons/fa";
 import { FaLaptopCode } from "react-icons/fa";
